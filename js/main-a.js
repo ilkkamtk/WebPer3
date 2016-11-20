d0668f61341110713eb755a11f5155100007fb27
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2016-11-21 01:06:15 +0200

    update

---
+++ js/main-a.js
@@ -0,0 +1,9 @@
+// tee XMLHttpRequest-objekti. Anna sen nimeksi xhr
+
+// tee funktio 'showImages', joka
+// tarkastaa onko readyState ja status sellaiset että ladatun sisällön voi näyttää, sekä 
+// lisää ladatun HTML-sisällön <ul> elementin sisälle
+
+// avaa XMLHttpRequest-yhteys osoitteeseen X, metodi GET
+// kun readystate vaihtuu, kutsu showImages funktiota
+// lähetä XMLHttpRequest-pyyntö
